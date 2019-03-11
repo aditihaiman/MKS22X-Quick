7b1ddc8104136767a71decc23f3d748fb6a23119
aditihaiman <ahaiman00@stuy.edu>
2019-03-11 16:23:53 -0400

    testing quickselect

--- Driver.java
+++ Driver.java
@@ -5,9 +5,10 @@ public class Driver {
 
   public static void main(String[] args) {
     int[] arr = {9, 6, 4, 5, 3, 8, 2, 7, 0, 1};
-    System.out.println(Arrays.toString(arr));
-    System.out.println(Quick.partition(arr, 0, 9));
-    System.out.println(Arrays.toString(arr));
+    // System.out.println(Arrays.toString(arr));
+    // System.out.println(Quick.partition(arr, 0, 9));
+    // System.out.println(Arrays.toString(arr));
+    System.out.println(Quick.quickselect(arr, 5));
 
   }
 
