75b63a7dcd46aa09696f429e27b19072e491fcb0
aditihaiman <ahaiman00@stuy.edu>
2019-03-11 12:26:42 -0400

    transferring partition method

---
+++ Quick.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Quick{
+
+
+
+
+  public static int partition(int[] data, int start, int end) {
+    int pivot = (int)(Math.random()*(end-start+1));
+    int temp = data[0];
+    data[0] = data[pivot];
+    data[pivot] = temp;
+    start = 1;
+    while(start<data.length&&start!=end){
+      if(data[start] > data[0]) {
+        temp = data[end];
+        data[end] = data[start];
+        data[start] = temp;
+        end--;
+      }
+      else start++;
+    }
+    //switch pivot element back to correct spot
+    if(data[0]>=data[end]) {
+      temp = data[0];
+      data[0] = data[end];
+      data[end] = temp;
+      return end;
+    }
+    else {
+      temp = data[0];
+      data[0] = data[end-1];
+      data[end-1] = temp;
+      return end-1;
+    }
+  }
+
+
+
+
+
+}
