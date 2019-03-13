01314e6a530af5afc93271573f1c9cdd85de1a86
aditihaiman <ahaiman00@stuy.edu>
2019-03-13 18:45:20 -0400

    testing quicksort with very large numbers

--- Driver.java
+++ Driver.java
@@ -4,12 +4,25 @@ import java.io.*;
 public class Driver {
 
   public static void main(String[] args) {
-    int[] arr = {-6, 1, -3, 2, 3, 5, 6, 8, 9};
-     System.out.println(Arrays.toString(arr));
-    System.out.println(Quick.partition(arr, 1, 2));
+    Random rand = new Random();
+
+    int[] arr = new int[10],arr2 = new int[10];
+    for(int x = 0; x < arr.length; x++) {
+      int temp = rand.nextInt(1000);
+      arr[x] = temp;
+      arr2[x] = temp;
+    }
     //System.out.println(Arrays.toString(arr));
-    //Quick.quicksort(arr);
-    System.out.println(Arrays.toString(arr));
+
+    Quick.quicksort(arr);
+    Arrays.sort(arr2);
+
+    for(int x = 0; x < arr.length; x++) {
+      if(arr[x]!=arr2[x]) System.out.println("WRONG");
+    }
+
+    //System.out.println(Arrays.toString(arr));
+
 
   }
 
